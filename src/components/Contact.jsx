2df4e289e00cec7ba9a7d76a47b91d4601273cbe
alvaroormeno <alvaro@ormeno.org>
2022-06-14 22:45:40 -0400

    working on contact page 30% completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+
+    // PAGE CONTAINER
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+        <form action="" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-[#80ED99] text-gray-300'>Contact</p>
+                <p className='text-gray-300 py-4'> Submit the form below or send me an email - alvaro@ormeno.org</p>
+            </div>
+            <input type="text" placeholder='Name' name='name' />
+            <input type="email" placeholder='E-mail' name='email' />
+        </form>
+
+    </div>
   )
 }
 
