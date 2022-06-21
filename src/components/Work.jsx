80a18b597c5fc7bdd44d404b3a45fb7e061f75e0
alvaroormeno <alvaro@ormeno.org>
2022-06-20 20:17:35 -0400

    added new work project, added work descrptions, and develop branch

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Flight from '../assets/flightfortonight.jpg'
 import Shopper from '../assets/consciousshopper.jpg'
+import airbnb from '../assets/airbnblanding.png'
 
 const Work = () => {
   return (
@@ -22,11 +23,13 @@ const Work = () => {
                 <div style={{backgroundImage: `url(${Flight}`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
                     {/* Hover Effect */}
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    <div className='opacity-0 group-hover:opacity-100 max-w-[400px]'>
                         <span className='text-2x1 font-bold text-white tracking-wider'>
                             Flight For Tonight
-
                         </span>
+                        <p>
+                        A simple, easy to use app that filters flight tickets based on your destination and amount of money you want to spend.
+                        </p>
                         <div className='pt-8 text-center'>
                             <a href="https://gallolopez1.github.io/flight-for-tonight/" target="_blank">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
@@ -47,11 +50,13 @@ const Work = () => {
                 <div style={{backgroundImage: `url(${Shopper}`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     
                     {/* Hover Effect */}
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    <div className='opacity-0 group-hover:opacity-100 max-w-[400px] '>
                         <span className='text-2x1 font-bold text-white tracking-wider'>
                             Conscious Shopper
-
                         </span>
+                        <p>
+                        App that lets customers and business owners search, review, vote and share small family and minority owned businesses in their area. This app is an SPA powered by MERN.
+                        </p>
                         <div className='pt-8 text-center'>
                             <a href="https://enigmatic-basin-52191.herokuapp.com/" target="_blank">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
@@ -59,6 +64,34 @@ const Work = () => {
                                 </button>
                             </a>
                             <a href="https://github.com/alvaroormeno/conscious-shopper" target="_blank">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Github Repo
+                                </button>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                {/* CARD 3 */}
+                <div style={{backgroundImage: `url(${airbnb}`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/* Hover Effect */}
+                    <div className='opacity-0 group-hover:opacity-100 max-w-[400px]'>
+                        <span className='text-2x1 font-bold text-white tracking-wider'>
+                            Mock Airbnb Landing Page
+                        </span>
+                        <p>
+                            A simple mockup of an Airbnb landing page constructed fully with React and TailwindCSS.
+                        </p>
+                        <div className='pt-8 text-center'>
+                            <a href="https://alvaroormeno.github.io/travel-rental-landing-page/" target="_blank">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Live Demo
+                                </button>
+                            </a>
+                            <a href="https://github.com/alvaroormeno/travel-rental-landing-page" target="_blank">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                                     Github Repo
                                 </button>
