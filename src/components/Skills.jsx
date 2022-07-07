25ad3d796b67e59c214015b3124de9dc75643025
alvaroormeno <alvaro@ormeno.org>
2022-07-07 14:02:00 -0400

    fixed and added skills, added mern to do app to work

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,19 +5,24 @@ import CSS from '../assets/css.png'
 import JavaScript from '../assets/javascript.png'
 import Node from '../assets/node.png'
 import ReactImage from '../assets/react.png'
+import Mongodb from '../assets/mongodb.png'
+import Tailwind from '../assets/tailwind.png'
+import Heroku from '../assets/heroku.png'
+import Express from '../assets/express.png'
 
 
 const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
         {/* MAIN CONTAINER */}
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full '>
             <div>
                 <p className='text-4xl inline border-b-4 border-[#80ED99]'>Skills</p>
                 <p className='py-4'>These are the technologies I've worked with</p>
             </div>
 
-            <div className='w-full grid grid-cols-2 sm:grid-cols-6 text-center py-8  '>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-6 text-center py-8 gap-y-2 gap-x-2  '>
+
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={HTML} alt="Html icon" />
                     <p className='my-4'>HTML</p>
@@ -42,6 +47,23 @@ const Skills = () => {
                     <img className='w-20 mx-auto' src={Node} alt="Html icon" />
                     <p className='my-4'>NODE</p>
                 </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Mongodb} alt="Html icon" />
+                    <p className='my-4'>MONGO DB</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Tailwind} alt="Html icon" />
+                    <p className='my-4'>TAILWINDCSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Heroku} alt="Html icon" />
+                    <p className='my-4'>HEROKU</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Express} alt="Html icon" />
+                    <p className='my-4'>EXPRESS.JS</p>
+                </div>
+
             </div>
 
         </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,6 +5,7 @@ import airbnb from '../assets/airbnblanding.png'
 import udemy from '../assets/udemy.png'
 import weatherapp from '../assets/weatherapp.png'
 import limanjar from '../assets/limanjarMain.png'
+import todoapp from '../assets/todoapp.png'
 
 const Work = () => {
   return (
@@ -16,11 +17,39 @@ const Work = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#80ED99]'>Work</p>
-                <p className='py-6'>Check out some of my work</p>
+                <p className='py-6'>Check out some of my recent projects!</p>
             </div>
 
             {/* CONTAINER FOR CARDS */}
             <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-4'>
+
+
+                {/* CARD MERN TO DO APP */}
+                <div style={{backgroundImage: `url(${todoapp}`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    
+                    {/* Hover Effect */}
+                    <div className='opacity-0 group-hover:opacity-100 max-w-[400px]'>
+                        <span className='text-2x1 font-bold text-white tracking-wider'>
+                            MERN ToDo App
+                        </span>
+                        <p>
+                        A full mobile responsive MERN stack To Do App that lets you login, logout and save your todos online.
+                        </p>
+                        <div className='pt-8 text-center'>
+                            <a href="https://aoz-mern-todo.herokuapp.com/" target="_blank">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Live Demo
+                                </button>
+                            </a>
+                            <a href="https://github.com/alvaroormeno/mern-to-do-app" target="_blank">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Github Repo
+                                </button>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
 
                 {/* CARD1 */}
                 <div style={{backgroundImage: `url(${Flight}`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
