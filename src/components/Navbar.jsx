26d8eb75fd871fdb5c21d47cc4c72f18bdc525a9
alvaroormeno <alvaro@ormeno.org>
2022-07-28 16:49:34 -0400

    changed work to projects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                 </li>
                 <li >
                     <Link to="work" smooth={true}  duration={500}>
-                        Work
+                        Projects
                     </Link>
                 </li>
                 <li >
@@ -84,7 +84,7 @@ const Navbar = () => {
             </li>
             <li className='py-6 text-4xl'>
                 <Link onClick={handleClick} to="work" smooth={true}  duration={500}>
-                    Work
+                    Projects
                 </Link>
             </li>
             <li className='py-6 text-4xl'>
